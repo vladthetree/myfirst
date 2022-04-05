26c8a9563fd4fb5dd48dbee7770c732bbfb3f20d
Massenwahnsinn <vladysspam@gmx.de>
2022-04-05 17:44:37 +0200

    x

--- 01_module/programm.js
+++ 01_module/programm.js
@@ -4,6 +4,7 @@ var dt = require('./myfirstmodule'); // ./ -> die File ist im selben Ordner wie 
 http.createServer(function (req, res) {
     // res.writeHead(200, {'Content-Type': 'text/plain'});
     // res.end('Hello World!');
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time are currently : " + dt.myDateTime());
 
